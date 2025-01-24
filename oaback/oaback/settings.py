5b26bb4e5e5ad8ef30d584725d59bfcd56b74c71
shi_guowei <shiguowei333@outlook.com>
2025-01-24 16:02:05 +0800

    增加跨域请求配置，开发阶段默认允许所有域名访问

--- oaback/oaback/settings.py
+++ oaback/oaback/settings.py
@@ -38,17 +38,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# 允许所有域名跨域访问
+CORS_ALLOWED_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'oaback.urls'
 
