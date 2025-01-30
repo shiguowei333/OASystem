ee1aa7414d58747b595183de4fe45605630bdaa6
shi_guowei <shiguowei333@outlook.com>
2025-01-30 20:32:59 +0800

    前端登录功能实现请求

--- oafront/src/views/login/login.vue
+++ oafront/src/views/login/login.vue
@@ -9,14 +9,14 @@
                     <span class="login100-form-title"> 员工登陆 </span>
 
                     <div class="wrap-input100 validate-input">
-                        <input class="input100" type="text" name="email" placeholder="邮箱" />
+                        <input class="input100" type="text" name="email" placeholder="邮箱" v-model="form.email" />
                         <span class="focus-input100"></span>
                         <span class="symbol-input100">
                             <i class="iconfont icon-fa-envelope" aria-hidden="true"></i>
                         </span>
                     </div>
                     <div class="wrap-input100 validate-input">
-                        <input class="input100" type="password" name="password" placeholder="密码" />
+                        <input class="input100" type="password" name="password" placeholder="密码" v-model="form.password" />
                         <span class="focus-input100"></span>
                         <span class="symbol-input100">
                             <i class="iconfont icon-fa-lock" aria-hidden="true"></i>
@@ -35,6 +35,38 @@
 
 <script setup name="login">
     import login_image from '@/assets/image/login.png'
+    import { reactive } from 'vue'
+    import axios from 'axios'
+
+    let form = reactive({
+        email: '',
+        password: ''
+    })
+
+    const onSubmit = () => {
+        let pwdRgx = /^[0-9a-zA-Z_-]{6,20}/
+        let emailRgx = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9])+/
+        if(!emailRgx.test(form.email)) {
+            alert('邮箱格式不满足！')
+            return
+        }
+        if(!pwdRgx.test(form.password)) {
+            alert('密码格式不满足！')
+            return
+        }
+        axios.post('http://127.0.0.1:8000/auth/login',{
+            email: form.email,
+            password: form.password
+        }).then((res) => {
+            let data = res.data
+            let token = data.token
+            let user = data.user
+            console.log(token,user)
+        }, (err) => {
+            let detail = err.response.data.detail
+            alert(detail)
+        })
+    }
 </script>
 
 <style scoped src="@/assets/css/login.css"></style>
