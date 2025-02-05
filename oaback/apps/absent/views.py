ce0740ee484699e79ad4a6dba6da0ce15cd038ba
shi_guowei <shiguowei333@outlook.com>
2025-02-05 16:40:44 +0800

    获取本人和下属的请假数据接口开发

--- oaback/apps/absent/views.py
+++ oaback/apps/absent/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+
 from .models import Absent, AbsentType, AbsentStatusChoices
 from .serializers import AbsentSerializer
 
@@ -13,3 +15,14 @@ class AbsentViewSet(mixins.CreateModelMixin,
     def update(self, request, *args, **kwargs):
         kwargs['partial'] = True
         return super().update(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        who = request.query_params.get('who')
+        if who and who == 'sub':
+            result = queryset.filter(responder=request.user)
+        else:
+            result = queryset.filter(requester=request.user)
+
+        serializer =  self.serializer_class(result, many=True)
+        return Response(data=serializer.data)
