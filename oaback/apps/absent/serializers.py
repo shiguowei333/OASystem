aa6139c34ad1ed558473e7f3c13e78ea10e05ea7
shi_guowei <shiguowei333@outlook.com>
2025-02-05 15:45:34 +0800

    完成考勤相关的序列化功能

--- oaback/apps/absent/serializers.py
+++ oaback/apps/absent/serializers.py
@@ -1,10 +1,12 @@
-from rest_framework import serializers
+from logging import exception
+
+from rest_framework import serializers, exceptions
 from apps.oaauth.serializers import UserSerializer
-from apps.absent.models import Absent
+from apps.absent.models import Absent, AbsentType, AbsentStatusChoices
 
 class AbsentTypeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Absent
+        model = AbsentType
         fields = '__all__'
 
 class AbsentSerializer(serializers.ModelSerializer):
@@ -16,8 +18,35 @@ class AbsentSerializer(serializers.ModelSerializer):
         model = Absent
         fields = '__all__'
 
+    def validate_type_id(self, value):
+        if not AbsentType.objects.filter(pk=value).exists():
+            raise exceptions.ValidationError('请假类型不存在')
+        return value
     def create(self, validated_data):
-        pass
+        request = self.context['request']
+        user = request.user
+        if user.department.leader.uid == user.uid:
+            if user.department.name == '董事会':
+                responder = None
+            else:
+                responder = user.department.manager
+        else:
+            responder = user.department.leader
+
+        if responder is None:
+            validated_data['status'] = AbsentStatusChoices.PASS
+
+        return Absent.objects.create(**validated_data, responder=responder, request=user)
+
 
     def update(self, instance, validated_data):
-        pass
+        if instance.status == AbsentStatusChoices.AUDITING:
+            raise exceptions.APIException('该请假单已审批')
+        request = self.context['request']
+        user = request.user
+        if instance.responder.uid != user.uid:
+            raise exceptions.AuthenticationFailed('您无权处理该考勤')
+        instance.status = validated_data.get('status', instance.status)
+        instance.response_content = validated_data['response_content']
+        instance.save()
+        return instance
--- oaback/apps/absent/views.py
+++ oaback/apps/absent/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from .models import Absent, AbsentType, AbsentStatusChoices
+from .serializers import AbsentSerializer
 
 class AbsentViewSet(mixins.CreateModelMixin,
                    mixins.UpdateModelMixin,
                    mixins.ListModelMixin,
                    viewsets.GenericViewSet):
     queryset = AbsentType.objects.all()
-    serializer_class = None
+    serializer_class = AbsentSerializer
